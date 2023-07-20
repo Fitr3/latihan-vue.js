c443d7751728fa63bcdd3abe768f8432002f693e
GeraAnggaraPutra <gera_098@smkassalaambandung.sch.id>
2023-07-20 15:45:03 +0700

    vue-project

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue'
+
+const msg = "Hello World"
+const rawHtml = "<li>Daftar 1</li>"
+const link = "https://www.youtube.com/"
+
+let a= 10
+let b= 5
+let c = a+b
+
+</script>
+
+<template>
+  <div id="app">
+    <span>{{ msg }}</span><br>
+    <span v-html="rawHtml"></span><br>
+    <button :disabled="isButtonDisabled">Button</button><br>
+    <span style="color:brown;">warna coklat</span><br>
+    <a v-bind:href="link"> youtube </a>
+
+    bil 1 = {{ a }}<br>
+    bil 2 = {{ b }}<br>
+    hasil bil 1 + bil 2 = {{ c }}
+  </div>
+</template>
