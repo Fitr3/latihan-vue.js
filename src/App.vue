1baebc3815651cb635a1de207fb805e8f73b568c
GeraAnggaraPutra <gera_098@smkassalaambandung.sch.id>
2023-07-21 16:15:31 +0700

    project-vue

--- src/App.vue
+++ src/App.vue
@@ -1,75 +1,103 @@
+
 <template>
   <div>
-    <h2>Formulir</h2>
-    <form @submit.prevent="submitForm">
-      <label for="">Nama</label>
-      <input class="{'is-invalid':isInvalidName}" 
-      type="text" name="nama" v-model="kontak.nama">
-      <div v-if="isInvalidName" class="error-message">
-        Nama Harus Di Isi!
-      </div>
+    <h1>Formulir</h1>
+    <form @submit.prevent="submitorm">
+      <label for="">Nama :  &nbsp; &nbsp; &nbsp;</label>
+      <input type="text" name="nama" id="nama" v-model="data.nama">
       <br>
-      <label for="">Email</label>
-      <input class="{'is-invalid':isInvalidEmail}"
-      type="email" name="email" v-model="kontak.email">
-      <div v-if="isInvalidEmail" class="error-message">
-        Email Harus Di Isi!
-      </div>
+      <label for="">Kelas : &nbsp; &nbsp; &nbsp; &nbsp;</label>
+      <input type="text" name="kelas" id="kelas" v-model="data.kelas">
+      <br>
+      <label for="">Alamat :&nbsp; &nbsp; &nbsp;</label>
+      <input type="text" name="alamat" id="alamat" v-model="data.alamat"> 
+      <br>
+      <label for="">Nilai MTK : </label>
+      <input type="number" name="mtk" id="mtk" v-model="data.mtk">
+      <br>
+      <label for="">Nilai IPA : &nbsp; &nbsp;</label>
+      <input type="number" name="ipa" id="ipa" v-model="data.ipa">
+      <br>
+      <label for="">Nilai B.indo : </label>
+      <input type="number" name="bindo" id="bindo" v-model="data.bindo">
+      <br> 
       <br>
       <button type="submit">Simpan</button>
+      <br>
+      <br>
+      <br>
     </form>
     <div v-if="showResult">
-      nama : {{ name }} <br>
-      email : {{ email }}
+      Nama : {{ name }} <br>
+      Kelas : {{ kelas }} <br>
+      Alamat : {{ alamat }} <br>
+      Nilai Mtk : {{ mtk }} <br>
+      Nilai Ipa : {{ ipa }} <br>
+      Nilai Bindo : {{ bindo }} <br>
+      Total Nilai : {{ mtk + ipa + bindo }} <br>
+      Status : {{ mtk + ipa + bindo >= 240 ? 'Lulus' : 'Tidak Lulus' }}
+
     </div>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue';
 
+
+import { ref } from 'vue';
 export default {
+  name: 'MyComponent',
   data() {
     return {
-      kontak: {
-        nama: '',
-        email: '',
-      },
+       data : 
+        {
+          name: '', 
+          kelas: '',
+          alamat: '',
+          mtk: '',
+          ipa: '',
+          bindo: ''
+
+        },
       showResult: ref(false),
-      name: ref(''),
-      email: ref(''),
-    };
-  },
-  computed: {
-    isInvalidName() {
-      return this.name.length <= 1;
+          name: '', 
+          kelas: '',
+          alamat: '',
+          mtk: '',
+          ipa: '',
+          bindo: '',
+          total: 0
+      };
     },
-    isInvalidEmail() {
-      return this.email.length <= 1;
-    }
-  },
-  methods: {
-    submitForm() {
-      this.name = this.kontak.nama;
-      this.email = this.kontak.email;
+
+      methods: {
+        submitorm() {
+      this.name = this.data.nama; 
+      this.kelas = this.data.kelas;
+      this.alamat = this.data.alamat;
+      this.mtk = this.data.mtk;
+      this.ipa = this.data.ipa;
+      this.bindo = this.data.bindo;
       this.showResult = true;
-      this.isInvalidEmail = false;
-      this.isInvalidName = false
-    },
-  },
-};
+         
+      }
+     }
+    }
+   
+     
 </script>
 
 <style>
-h2 {
-  color: black;
+h1 {
+  color: rgb(251, 251, 251);
 }
+
 .error-message{
-  color: red;
-  margin-top: 5px;
+  color:red;
+  margin-top: 5px
 }
 
 .is-invalid {
-  border-color: red;
+  border-color:red;
 }
 </style>
